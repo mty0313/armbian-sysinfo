219a28979b5be1a4b6cf59fc61e2cdaffd8fc30d
mty <mty0313@126.com>
2023-02-03 11:48:22 +0800

    init

---
+++ src/main/java/top/mty/ArmbianSysinfoApplication.java
@@ -0,0 +1,13 @@
+package top.mty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmbianSysinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmbianSysinfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/top/mty/ArmbianSysinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package top.mty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmbianSysinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
